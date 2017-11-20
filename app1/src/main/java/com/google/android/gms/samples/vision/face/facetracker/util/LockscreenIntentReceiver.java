5ec9c67ab6eee6f34c95d78448996aa9030c776b
ADITYANALWAYA <nalwayaadi@gmail.com>
2017-11-20 17:18:43 +0530

    First commit

---
+++ app1/src/main/java/com/google/android/gms/samples/vision/face/facetracker/util/LockscreenIntentReceiver.java
@@ -0,0 +1,34 @@
+package com.google.android.gms.samples.vision.face.facetracker.util;
+
+/**
+ * Created by admin on 20-11-2017.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.android.gms.samples.vision.face.facetracker.TrackerActivity;
+
+public class LockscreenIntentReceiver extends BroadcastReceiver {
+
+    // Handle actions and display Lockscreen
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)
+                || intent.getAction().equals(Intent.ACTION_SCREEN_ON)
+                || intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            start_lockscreen(context);
+        }
+
+    }
+
+    // Display lock screen
+    private void start_lockscreen(Context context) {
+        Intent mIntent = new Intent(context, TrackerActivity.class);
+        mIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(mIntent);
+    }
+
+}
